c506a7822062fea87e01911bea1bee7a041c0fed
jevgenij-p <eugene.pankov@gmail.com>
2021-07-30 22:11:13 +0300

    updated deployment script

---
+++ functions.ps1
@@ -0,0 +1,19 @@
+#
+# Common functions
+#
+
+function Get-RandomKey {
+    $length=32
+    $randomString=-join (((33..126)) * 100 | Get-Random -Count $length | %{[char]$_})
+    $bytes=[System.Text.Encoding]::Unicode.GetBytes($randomString)
+    $key=[Convert]::ToBase64String($bytes)
+    return $key
+}
+
+function Get-RandomLowercaseAndNumbers {
+    Param([int]$length)
+
+    # lowercase letters and numbers only
+    $randomString=-join (((48..57)+(97..122)) * 100 | Get-Random -Count $length | %{[char]$_})
+    return $randomString
+}
